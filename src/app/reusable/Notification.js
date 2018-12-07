30ef997c27ce606ff555d38085e5d6013e28d91f
Davi Spindola <davi@arquia.com.br>
2018-12-07 04:34:04 -0200

    feat: implementing component for user actions notifications

---
+++ src/app/reusable/Notification.js
@@ -0,0 +1,52 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { compose } from "recompose";
+import { connect } from "react-redux";
+import { Snackbar } from "@material-ui/core";
+
+class Notification extends React.Component {
+  render() {
+    const { message, open, handleClose } = this.props;
+
+    return (
+      <Snackbar
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right"
+        }}
+        open={open}
+        onClose={handleClose}
+        autoHideDuration={4000}
+        message={<span>{message}</span>}
+      />
+    );
+  }
+}
+
+Notification.propTypes = {
+  message: PropTypes.string,
+  open: PropTypes.bool,
+  handleClose: PropTypes.func
+};
+
+Notification.defaultProps = {
+  message: "",
+  open: false,
+  handleClose: null
+};
+
+const mapStateToProps = state => ({
+  message: state.notificationState.message,
+  open: state.notificationState.open
+});
+
+const mapDispatchToProps = dispatch => ({
+  handleClose: () => dispatch({ type: "CLOSE_NOTIFICATION" })
+});
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(Notification);
