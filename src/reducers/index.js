7e36d654f3d367d1807efc3c70c5a165354cbb25
Davi Spindola <davi@arquia.com.br>
2018-12-07 04:34:19 -0200

    feat: implementing new reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,12 +4,14 @@ import sessionReducer from "./session";
 import userReducer from "./user";
 import feedReducer from "./feed";
 import mediaReducer from "./media";
+import notificationReducer from "./notification";
 
 const rootReducer = combineReducers({
   sessionState: sessionReducer,
   userState: userReducer,
   feedState: feedReducer,
-  mediaState: mediaReducer
+  mediaState: mediaReducer,
+  notificationState: notificationReducer
 });
 
 export default rootReducer;
